77b2630da68371107ce050aa30144867c2466ac4
rojit1000 <rojit.1000@gmail.com>
2023-02-16 11:37:16 +0545

    test commit before changing model names

--- purchase/forms.py
+++ purchase/forms.py
@@ -3,23 +3,66 @@ from .models import Vendor, ProductPurchase
 from root.forms import BaseForm
 from product.models import Product
 
-class VendorForm(forms.ModelForm):
+class VendorForm(BaseForm, forms.ModelForm):
     class Meta:
         model = Vendor
         fields = ['name', 'address', 'contact',]
     
 class ProductPurchaseForm(BaseForm, forms.ModelForm):
+    DISCOUNT_PERCENTAGE_CHOICES = (
+        (0,0),
+        (5,5),
+        (10, 10),
+        (20, 20),
+        (30, 30),
+        (40, 40),
+        (50, 50),
+
+    ) 
 
     vendor = forms.ModelChoiceField(
         queryset=Vendor.objects.active(),
     )
-
-
     sub_total = forms.FloatField()
+    discount_percentage = forms.ChoiceField(choices=DISCOUNT_PERCENTAGE_CHOICES)
+    discount_amount = forms.FloatField(initial=0.0)
+
+
+    taxable_amount = forms.FloatField(initial=0.0)
+    non_taxable_amount = forms.FloatField(initial=0.0)
+
+    tax_amount = forms.FloatField(initial=0.0)
     grand_total = forms.FloatField()
+    amount_in_words = forms.CharField()
+    payment_mode = forms.ChoiceField(
+        choices=[
+            ("", "-----------------"),
+            ("Cash", "Cash"),
+            ("Credit", "Credit"),
+            ("Credit Card", "Credit Card"),
+            ("Mobile Payment", "Mobile Payment"),
+            ("Complimentary", "Complimentary"),
+        ],
+        required=True,
+    )
 
 
-    field_order = ['vendor', 'product', 'sub_total' ,'grand_total']
+    field_order = ['vendor', 'product', 'sub_total', 'discount_percentage', 'discount_amount', 'taxable_amount',
+                'non_taxable_amount', 'tax_amount', 'grand_total', 'amount_in_words', 'payment_mode']
+
+
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["sub_total"].widget.attrs["readonly"] = True
+        self.fields["taxable_amount"].widget.attrs["readonly"] = True
+        self.fields["non_taxable_amount"].widget.attrs["readonly"] = True
+        self.fields["tax_amount"].widget.attrs["readonly"] = True
+        self.fields["grand_total"].widget.attrs["readonly"] = True
+        self.fields["tax_amount"].label = "VAT Amount"
+        self.fields["discount_amount"].widget.attrs["readonly"] = True
+        self.fields["amount_in_words"].widget.attrs["readonly"] = True
+
 
     class Meta:
         model = ProductPurchase
--- purchase/models.py
+++ purchase/models.py
@@ -6,25 +6,33 @@ class Vendor(BaseModel):
     name = models.CharField(max_length=50)
     address = models.CharField(max_length=100, null=True, blank=True)
     contact = models.CharField(max_length=10, null=True, blank=True)
+    # pan_no = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return self.name
+
 
 class Purchase(BaseModel):
     vendor = models.ForeignKey(Vendor, on_delete=models.SET_NULL, null=True)
     products = models.ManyToManyField(Product, through='ProductPurchase')
     purchase_date = models.DateField(auto_now=True, blank=True)
-    grand_amount = models.DecimalField(max_digits=9, decimal_places=2, null=True, blank=True)
+    sub_total = models.DecimalField(max_digits=9, decimal_places=2)
+    grand_total = models.DecimalField(max_digits=9, decimal_places=2)
+    taxable_amount = models.DecimalField(max_digits=9, decimal_places=2, null=True, blank=True)
+    non_taxable_amount = models.DecimalField(max_digits=9, decimal_places=2, null=True, blank=True)
+    tax_amount = models.DecimalField(max_digits=9, decimal_places=2, null=True, blank=True)
+    amount_in_words = models.CharField(max_length=255)
+    payment_mode = models.CharField(max_length=30)
 
     def __str__(self):
-        return f'Purchased from {self.vendor.name} total = {self.grand_amount}'
+        return f'Purchased from {self.vendor.name} total = {self.grand_total}'
 
 
 
 class ProductPurchase(BaseModel):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     purchase = models.ForeignKey(Purchase, on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=9, decimal_places=2, null=True, blank=True)
+    rate = models.DecimalField(max_digits=9, decimal_places=2, null=True, blank=True)
     quantity = models.IntegerField()
     item_total = models.DecimalField(max_digits=9, decimal_places=2, null=True, blank=True)
 
